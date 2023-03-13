f834bfd4c3c6c333e11036d2ad1d46108cd1e949
roxania <groxania@gmail.com>
2023-03-12 22:08:17 -0300

    haciendo caja ejercicio 3 array

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -30,66 +30,131 @@ function ejer3() {
         switch (sumas[index]) {
             case 1:
                 contador1 = contador1 + 1;
-                contador[1] = ["en la suma el numero 1 se encontro: " + contador1 + " veces <br>"];
+                if (contador1 == 1) {
+                    contador[1] = ["<br>El numero 1 se encontro " + contador1 + " ves"];
+                } else {
+                    contador[1] = ["<br>El numero 1 se encontro " + contador1 + " veces"];
+                }
+
 
                 break;
             case 2:
                 contador2 = contador2 + 1;
-                contador[2] = ["en la suma el numero 2 se encontro: " + contador2 + " veces <br>"];
+                if (contador2 == 1) {
+                    1
+                    contador[2] = ["<br>El numero 2 se encontro " + contador2 + " ves"];
+                } else {
+                    contador[2] = ["<br>El numero 2 se encontro " + contador2 + " veces "];
+                }
+
 
                 break;
             case 3:
                 contador3 = contador3 + 1;
-                contador[3] = ["en la suma el numero 3 se encontro: " + contador3 + " veces <br>"];
+                if (contador3 == 1) {
+                    contador[3] = ["<br>El numero 3 se encontro " + contador3 + " ves"];
+                } else {
+                    contador[3] = ["<br>El numero 3 se encontro " + contador3 + " veces"];
+                }
+
 
                 break;
             case 4:
                 contador4 = contador4 + 1;
-                contador[4] = ["en la suma el numero 4 se encontro: " + contador4 + " veces<br>"];
+                if (contador4) {
+                    contador[4] = ["<br>El numero 4 se encontro " + contador4 + " ves"];
+                } else {
+                    contador[4] = ["<br>El numero 4 se encontro " + contador4 + " veces"];
+                }
+
 
                 break;
             case 5:
                 contador5 = contador5 + 1;
-                contador[5] = ["en la suma el numero 5 se encontro: " + contador5 + " veces<br>"];
-
+                if (contador5 == 1) {
+                    contador[5] = ["<br>El numero 5 se encontro " + contador5 + " ves"];
+                } else {
+                    contador[5] = ["<br>El numero 5 se encontro " + contador5 + " veces"];
+                }
                 break;
             case 6:
                 contador6 = contador6 + 1;
-                contador[6] = ["en la suma el numero 6 se encontro: " + contador6 + " veces<br>"];
+                if (contador6 == 1) {
+                    contador[6] = ["<br>El numero 6 se encontro " + contador6 + " ves"];
+                } else {
+                    contador[6] = ["<br>El numero 6 se encontro " + contador6 + " veces"];
 
+                }
                 break;
             case 7:
                 contador7 = contador7 + 1;
-                contador[7] = ["en la suma el numero 7 se encontro: " + contador7 + " veces<br>"];
+                if (contador7 == 1) {
+                    contador[7] = ["<br>El numero 7 se encontro " + contador7 + " ves"];
 
+                } else {
+                    contador[7] = ["<br>El numero 7 se encontro " + contador7 + " veces"];
+
+                }
                 break;
             case 8:
                 contador8 = contador8 + 1;
-                contador[8] = ["en la suma el numero 8 se encontro: " + contador8 + " veces<br>"];
+                if (contador8 == 1) {
+                    contador[8] = ["<br>El numero 8 se encontro " + contador8 + " ves"];
 
+                } else {
+                    contador[8] = ["<br>El numero 8 se encontro " + contador8 + " veces"];
+
+                }
                 break;
             case 9:
                 contador9 = contador9 + 1;
-                contador[9] = ["en la suma el numero 9 se encontro: " + contador9 + " veces<br>"];
+                if (contador9 == 1) {
+                    contador[9] = ["<br>El numero 9 se encontro " + contador9 + " ves"];
 
+                } else {
+                    contador[9] = ["<br>El numero 9 se encontro " + contador9 + " veces"];
+
+                }
                 break;
             case 10:
                 contador10 = contador10 + 1;
-                contador[10] = ["en la suma el numero 10 se encontro: " + contador10 + " veces<br>"];
+                if (contador10 == 1) {
+                    contador[10] = ["<br>El numero 10 se encontro " + contador10 + " ves"];
+
+                } else {
+                    contador[10] = ["<br>El numero 10 se encontro " + contador10 + " veces"];
+
+                }
 
                 break;
             case 11:
                 contador11 = contador11 + 1;
-                contador[11] = ["en la suma el numero 11 se encontro: " + contador11 + " veces<br>"];
+                if (contador11 == 1) {
+                    contador[11] = ["<br>El numero 11 se encontro " + contador11 + " ves"];
 
+                } else {
+                    contador[11] = ["<br>El numero 11 se encontro " + contador11 + " veces"];
+
+                }
                 break;
             case 12:
                 contador12 = contador12 + 1;
-                contador[12] = ["en la suma el numero 12 se encontro: " + contador12 + " veces<br>"];
-                break;
+                if (contador12 == 1) {
+                    contador[12] = ["<br>El numero 12 se encontro " + contador12 + " ves"];
+
+                } else {
+                    contador[12] = ["<br>El numero 12 se encontro " + contador12 + " veces"];
+
+                } break;
             default:
                 contador0 = contador0 + 1;
-                contador[0] = ["en la suma el numero 0 se encontro: " + contador0 + " veces<br>"];
+                if (contador0 == 1) {
+
+                    contador[0] = ["en la suma de cada par de dados :<br>El numero 0 se encontro " + contador0 + " ves"];
+                } else {
+                    contador[0] = ["en la suma de cada par de dados :<br>El numero 0 se encontro " + contador0 + " veces"];
+                }
+
         }
 
 
